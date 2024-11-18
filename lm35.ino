e3fd779216e227964ff82996fa9943ecc6d45d1f
roghayehoseiny <roghayehoseiny.2003@gmail.com>
2024-11-18 10:21:22 +0100

    Add files via upload

---
+++ lm35.ino
@@ -0,0 +1,24 @@
+const int lm35pin = A0;
+
+int led = 9;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(led, OUTPUT);
+}
+void loop() {
+  int sensorValue = analogRead(lm35pin);
+  float temprature = sensorValue * 0.48818225;
+  Serial.print("temprature=");
+  Serial.print(temprature);
+  Serial.println("*C");
+
+
+  if (temprature > 27){
+    Serial.print("cooler on");
+    digitalWrite(led, HIGH);
+  }else{
+    digitalWrite(led, LOW);
+  }
+  delay(1000);
+}
